1287b4f850512ecf70e743f757e9c5699ee57927
BestCherif <121627108+BestCherif@users.noreply.github.com>
2024-03-19 15:31:13 +0100

    Update DMSacADos.py
    Update2 to remove extra useless comments xd

--- DMSacADos.py
+++ DMSacADos.py
@@ -14,21 +14,12 @@ for j in range(1,n+1):#rotation n fois pour ajouter les n objets dans la list d'
         importance = float(input("SVP,Évaluer l'importance de l'objet sur 10 pour faciliter les choses: "))
     List.append(importance)
     Les_objets.append(List)
-
-
 Les_objets.sort(reverse=True, key=lambda objet: objet[2])
-#on a tri la matrice "Les_objets" D'apres l'importance de chaque objet en order decroissante
-#j'ai trouve la fonction anonymous lambda dans W3school website et sur ChatGPT
-#reverse= true , d'apres W3School website, pour le tri en ordre decroissante
-
-
 for o in range(0,n):# range (0.n) pour utiliser o comme indice
     if Les_objets[o][1] <= MaxP:
         sac.append(Les_objets[o])
         #on peut preciser qu'on ajoute que le nom de l'objet sans son poid et son importance en remplacant Les_objets[o] par Les_objets[o][0]
         MaxP = MaxP-Les_objets[o][1]
-
-
 print("Les objets qu'on peux mettre dans le sac sont: ", end='')
 #end='' pour qu'on peut "print" juste après celle-ci sans sauter une ligne
 for object in sac:
